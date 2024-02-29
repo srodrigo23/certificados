54d4e490225727a20e51c67d0777a76ad7face84
rodres101 <rodrigosergio93@gmail.com>
2024-02-28 21:37:33 -0400

    trying data base

--- app/__init__.py
+++ app/__init__.py
@@ -1,35 +1,36 @@
 from flask import Flask
 from app.extensions import db
 from config import config
-# from app.extensions import migrate
+from app.extensions import migrate
+
 import os
 
 def create_app():
     
     app = Flask(__name__)
-    # print(env_values['CONFIG_MODE'])
-    # app.config.from_object(config[os.getenv("CONFIG_MODE")])
-    # app.config['SQLALCHEMY_DATABASE_URI'] = env_values['DEVELOPMENT_DATABASE_URL']
     app.config.from_object(config[os.environ.get("CONFIG_MODE")])
     
     # Initialize Flask extensions here
-    # db.init_app(app)
+    db.init_app(app)
     # migrate.init_app(app, db)
 
-    #Register blueprints here
-    from app.main import bp as main_bp
-    app.register_blueprint(main_bp)
+    with app.app_context():
+        db.create_all()
 
-    # from app.posts import bp as posts_bp
-    # app.register_blueprint(posts_bp, url_prefix='/posts')
+        #Register blueprints here
+        from app.main import bp as main_bp
+        app.register_blueprint(main_bp)
 
-    # from app.questions import bp as questions_bp
-    # app.register_blueprint(questions_bp, url_prefix='/questions')
+        # from app.posts import bp as posts_bp
+        # app.register_blueprint(posts_bp, url_prefix='/posts')
 
-    @app.route('/test/')
-    def test_page():
-        return '<h1>Testing the Flask Application Factory Pattern</h1>'
+        # from app.questions import bp as questions_bp
+        # app.register_blueprint(questions_bp, url_prefix='/questions')
 
-    return app
+        @app.route('/test/')
+        def test_page():
+            return '<h1>Testing the Flask Application Factory Pattern</h1>'
+
+        return app
 
 
